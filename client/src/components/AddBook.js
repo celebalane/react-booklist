1d8a320c578ad25ef4939dfdcfcb8198636d842c
Emilie Leterme <celebalane@gmail.com>
2019-08-29 16:52:16 +0200

    Installation de react, queries pour afficher la liste et ajouter un livre

---
+++ client/src/components/AddBook.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import {flowRight as compose} from 'lodash';
+
+import { getAuthorsQuery, addBookMutation } from '../queries/queries';
+
+class AddBook extends Component {
+
+	constructor(props){
+		super(props);
+		this.state = {
+			name: "",
+			genre: "",
+			authorId: ""
+		};
+	}
+
+	displayAuthors(){
+		var data = this.props.getAuthorsQuery;
+		if(data.loading){
+			return (<option disabled>Chargement en cours</option>)
+		} else {
+			return data.authors.map(author =>{
+				return(<option key={ author.id } value={ author.id }>{ author.name }</option>)
+			})
+		}
+	}
+
+	submitForm(e){
+		e.preventDefault();
+		this.props.addBookMutation();
+	}
+
+	render() {
+		return (
+		    <form id="add-book" onSubmit={ this.submitForm.bind(this) }>
+		    	<div className="field">
+		    		<label>Titre:</label>
+		    		<input type="text" onChange={ (e) => this.setState({ name: e.target.value })} />
+		    	</div>
+		    	<div className="field">
+		    		<label>Genre:</label>
+		    		<input type="text" onChange={ (e) => this.setState({ genre: e.target.value })} />
+		    	</div>
+		    	<div className="field">
+		    		<label>Auteur</label>
+		    		<select onChange={ (e) => this.setState({ authorId: e.target.value })}>
+		    			<option>Veuillez choisir un auteur</option>
+		    			{ this.displayAuthors() }
+		    		</select>
+		    	</div>
+
+		    	<button>+</button>
+		    </form>
+  		);
+	}
+}
+
+export default compose(   //Permet de bind les différentes queries au component
+	graphql(getAuthorsQuery, { name: "getAuthorsQuery"}),
+	graphql(addBookMutation, { name: "addBookMutation"})
+)(AddBook);
--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
-
+const cors = require('cors'); 
 const app = express();
+
+//Autorise les requêtes sur des serveurs distincts
+app.use(cors());
 
 //connexion DB
 mongoose.connect("mongodb+srv://celebalane:password@cluster0-r41ji.mongodb.net/test?retryWrites=true&w=majority");
